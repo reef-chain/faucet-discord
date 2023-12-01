8dfce8349ada864247cdd1c3798c0803d9301ad7
Anukul Pandey <anukul.030601@gmail.com>
2023-12-01 15:04:31 +0530

    feat: prod config

---
+++ src/config/env/production.js
@@ -0,0 +1,14 @@
+"use strict";
+
+module.exports = {
+    token: "MTE1MzY1NjM4NTEwODcwNTM1MQ.GrB3Is.xjT--UBOc2sXdJHPDDGOy62kepQsUjpt571krQ",
+    prefix: '/',
+    symbol: 'REEF',
+    decimals: 18,
+    ws: 'ws://rpc-testnet.reefscan.info/ws',
+    address_type: 42, 
+    mnemonic: 'control employ home citizen film salmon divorce cousin illegal episode certain olympic',
+    amount: 2000,
+    limit: 12,
+    clientId:'1153656385108705351'
+};

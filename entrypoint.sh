302888b051337afcd0e2637925755ce5ef85b61d
matjazonline <matjazonline@gmail.com>
2023-12-07 00:29:19 +0100

    fix entrypoint.sh url replace

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed -i "s/REPLACE_MNEMONIC/$MNEMONIC/g" ./dist/app.js
-sed -i "s/REPLACE_DISCORD_TOKEN/$DISCORD_TOKEN/g" ./dist/app.js
-sed -i "s/REPLACE_DISCORD_CLIENT_ID/$DISCORD_CLIENT_ID/g" ./dist/app.js
-sed -i "s/REPLACE_RPC_WSS/$RPC_WSS/g" ./dist/app.js
+sed -i "s|REPLACE_MNEMONIC|$MNEMONIC|g" ./dist/app.js
+sed -i "s|REPLACE_DISCORD_TOKEN|$DISCORD_TOKEN|g" ./dist/app.js
+sed -i "s|REPLACE_DISCORD_CLIENT_ID|$DISCORD_CLIENT_ID|g" ./dist/app.js
+sed -i "s|REPLACE_RPC_WSS|$RPC_WSS|g" ./dist/app.js
 yarn start
